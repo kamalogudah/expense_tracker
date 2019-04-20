a9d441ad3850b807b2ef9e8b669579a8f0e4aa14
kamalogudah <mcpaul2058@gmail.com>
2019-04-20 16:57:07 +0300

    Defining the Ledger

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -2,7 +2,7 @@ require_relative "../../../app/api"
 require "rack/test"
 
 module ExpenseTracker
-  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+  #   RecordResult = Struct.new(:success?, :expense_id, :error_message)
   RSpec.describe API do
     include Rack::Test::Methods
 
@@ -13,9 +13,8 @@ module ExpenseTracker
     let(:ledger) { instance_double("ExpenseTracker::Ledger") }
 
     describe "POST /expenses" do
-      let(:expense) { { "some" => "data" } }
-
       context "when the expense is successfully recorded" do
+        let(:expense) { { "some" => "data" } }
         before do
           allow(ledger).to receive(:record)
                              .with(expense)
@@ -33,8 +32,21 @@ module ExpenseTracker
       end
 
       context "when the expense fails validation" do
-        it "returns an error message"
-        it "responds with a 422 (Unprocessable enity"
+        let(:expense) { { "some" => "data" } }
+        before do
+          allow(ledger).to receive(:record)
+                             .with(expense)
+                             .and_return(RecordResult.new(false, 417, "Expense incomplete"))
+        end
+        it "returns an error message" do
+          post "/expenses", JSON.generate(expense)
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include("error" => "Expense incomplete")
+        end
+        it "responds with a 422 (Unprocessable enity" do
+          post "/expenses", JSON.generate(expense)
+          expect(last_response.status).to eq(422)
+        end
       end
     end
   end
