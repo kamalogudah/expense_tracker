d7ae6748658ab4916b9a560ee08fe6605bac8db8
kamalogudah <mcpaul2058@gmail.com>
2019-04-23 15:07:37 +0300

    Isolating Your Specs Using Database Transactions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,6 +45,9 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.filter_gems_from_backtrace "rack", "rack-test", "sequel", "sinatra"
+  config.when_first_matching_example_defined(:db) do
+    require_relative "./support/db"
+  end
 
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
